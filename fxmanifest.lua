c5fae3004160c0de8222f0b3dbb00f96c1b9217d
YUPPZWORKSHOPCFX <129989756+YUPPZWORKSHOPCFX@users.noreply.github.com>
2025-07-29 15:24:48 +0700

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,22 @@
+-- NC PROTECT+
+shared_scripts { '@nc_PROTECT+/exports/protected.lua', '@nc_PROTECT+/exports/sh.lua' }
+
+author 'YUPPZWORKSHOPCFX {MODIFY}'
+
+fx_version 'adamant'
+
+game 'gta5'
+
+lua54 'yes'
+
+shared_script {
+    'config.lua'
+}
+
+client_scripts {
+	'client/client.lua'
+}
+
+server_scripts {
+	'server/server.lua'
+}
